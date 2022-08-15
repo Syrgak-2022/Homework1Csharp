751431c7992440a6141805a7e7827b83609493c1
Сыргак Иманкулов <syrgakimankulov@gmail.com>
2022-08-16 00:47:59 +0300

    Add first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача #2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// Console.WriteLine("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+ 
+// if(num1 > num2)
+//     {
+//         Console.WriteLine("Первое число больше второго");
+//     }
+// else if (num1 < num2)
+//     {
+//         Console.WriteLine("Первое число меньше второго");
+//     }
+// else
+//     {
+//         Console.WriteLine("Оба числа равны");
+//     }
+
+
+
+
+// Задача #4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+// int a = 2;
+// int b = 3;
+// int c = 7;
+// int max = a;
+// if (a > max) max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+// Console.Write("max - ");
+// Console.WriteLine(max);
